2fcbd71b306b036609991352db5360564c592292
DESKTOP-PEFJP14\zaks <glcasian@outlook.com>
2022-09-02 17:33:06 +0800

    主页添加计数器

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int num = 0;
+
+  void increment() => setState(() => num++);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [Text("主页")],
-      ),
-    );
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("$num",style: const TextStyle(fontSize: 30,color: Colors.blue),)],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
+        ));
   }
 }
